acffa7ffa0cd699dbde5629574a4f76ef54de20f
armyUser3181 <yong3181.25.1@gmail.com>
2026-07-06 13:25:45 +0000

    push

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # C_lesson_Box
 crazy
+
+## 다음에 계속
+
+YED를 활용한 define 객체를 생성합니다
+
+YED 리턴값은 다음 시퀀스의 여부를 결정합니다.
+
+stack up -> up; switch( main() ) 
+
+function (): for {
+    switch (main()) {
+        case up: function;
+        case dewn break;
+    }
+    return;
+}
--- main.c
+++ main.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+#define SEQ static _Thread_local unsigned long long int SEQ__POINT__LINE = __LINE__; switch( SEQ__POINT__LINE ) { case __LINE__:
+#define YED( value ) SEQ__POINT__LINE = __LINE__; return value; case __LINE__:
+#define END }
+
+
 typedef int (*LoopFunction)(int argc, char** argv);
 
 typedef struct {
@@ -23,7 +28,23 @@ int run() {
 
 }
 
-int main(int argc, char** argv) {
+int seqTast() {
+    SEQ
+        printf("hello world 1 \n");
+        YED( 1 )
+        printf("hello world 2 \n");
+        YED( 2 )
+        printf("hello world 3 \n");
+        YED( 3 )
+        printf("hello world 4 \n");
+    END
+        return 4;
+}
 
+int main(int argc, char** argv) {
+    for(int i = 0; i < 5; i++) {
+        seqTast();
+    }
+    return 0;
 }
 
