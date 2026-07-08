338049b59afba005fbe3884b03e18999a781bede
armyUser3181 <yong3181.25.1@gmail.com>
2026-07-08 14:03:32 +0000

    goto

--- Debugger/Debugger.c
+++ Debugger/Debugger.c
@@ -1,41 +1,92 @@
 
 #include "Debugger.h"
 #include <stdarg.h>
+#include <string.h>
 
 #define SET_DEBUGGER_METHOD_NAME(name) __DEBUGGERTOOL__FUNCTION__##name
 
-static size_t debugMode = 0;
-static char* debugMemory = NULL;
+static struct {
+    size_t debugMode;
+    struct {
+        size_t line;
+    } engine;
+    struct {
+        size_t top;
+        size_t size;
+        char* ptr;
+    } memory;
+} debug_context = {
+    .debugMode = 0,
+};
 
-static void SET_DEBUGGER_METHOD_NAME(setMemory)(char* ptr) {
-    
-}
+static void debug_printf_engine(int* ch) {
+    switch( debug_context.engine.line ) {
+        
+        if( strchr("-+0 #", *ch) != NULL ) {
 
-static size_t SET_DEBUGGER_METHOD_NAME(log)(const char* format, ...) {
-    if(debugMode == 0) return 0;
-    char* corser = format;
-    size_t line = 0;
-    while(*corser) {
-        int hcode = *corser++;
+        } else { debug_context.engine.line = __LINE__; break; } case __LINE__:
+        if( strchr("1234567890", *ch) != NULL ) {
 
-        switch(line) {
-            default:
+        } else { debug_context.engine.line = __LINE__; break; } case __LINE__:
+        if( strchr(".", *ch) != NULL ) {
 
+        } else { debug_context.engine.line = __LINE__; break; } case __LINE__:
+        if( strchr("-+0 #", *ch) != NULL ) {
 
-            if( hcode != '%' ) continue;
-            line = __LINE__; break; case __LINE__:
+        } else { debug_context.engine.line = __LINE__; break; } case __LINE__:
+        if( strchr("hll", *ch) != NULL ) {
 
-
-        }
+        } else { debug_context.engine.line = __LINE__; break; } case __LINE__:
+        //debug_context.engine.line = __LINE__; break; case __LINE__:
+        //debug_context.engine.line = __LINE__; break; case __LINE__:
+        //debug_context.engine.line = __LINE__; break; case __LINE__:
     }
 }
 
+
+
+
+
+static void SET_DEBUGGER_METHOD_NAME(setMemory)(char* ptr, size_t size) {
+    debug_context.memory.ptr = ptr;
+    debug_context.memory.size = size;
+}
+
+static size_t SET_DEBUGGER_METHOD_NAME(log)(const char* format, ...) {
+    if(debug_context.debugMode == 0) return 0;
+    char* corser_low = format;
+    char* corser_high = format;
+    size_t line = 0;
+    while(*corser_high) {
+        int* hcode = corser_high++;
+
+        switch(line) {
+            default:
+            if( *hcode != '%' ) continue;
+            {
+                char* ptr = debug_context.memory.ptr + debug_context.memory.top;
+                size_t max_size = debug_context.memory.size - debug_context.memory.top;
+                size_t size = hcode - corser_low - 1;
+                if( max_size < size ) {
+                    puts("error: out of memory in debugger ");
+                    return 0;
+                }
+                memcpy(ptr, corser_low, size);
+                debug_context.memory.top += size;
+            }
+            line = __LINE__; break; case __LINE__:
+
+        }
+    }
+
+}
+
 static void SET_DEBUGGER_METHOD_NAME(start)() {
-    debugMode = 1;
+    debug_context.debugMode = 1;
 }
 
 static void SET_DEBUGGER_METHOD_NAME(end)() {
-    debugMode = 0;
+    debug_context.debugMode = 0;
 }
 
 fdebugger getKitDebugger() {
@@ -43,6 +94,7 @@ fdebugger getKitDebugger() {
         .log = SET_DEBUGGER_METHOD_NAME(log),
         .start = SET_DEBUGGER_METHOD_NAME(start),
         .end = SET_DEBUGGER_METHOD_NAME(end),
+        .setMemory = SET_DEBUGGER_METHOD_NAME(setMemory),
     };
     return kit;
 }
--- Debugger/Debugger.h
+++ Debugger/Debugger.h
@@ -5,6 +5,7 @@ typedef struct fdebugger {
     size_t (*log)(const char*, ...);
     void (*start)();
     void (*end)();
+    void (*setMemory)(char*, size_t);
 } fdebugger;
 
 extern fdebugger getKitDebugger();
