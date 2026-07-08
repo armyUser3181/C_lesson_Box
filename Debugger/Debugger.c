2fc57217b0e828c015f645077e96aac4746c3d05
armyUser3181 <yong3181.25.1@gmail.com>
2026-07-08 03:53:21 +0000

    goto

--- Debugger/Debugger.c
+++ Debugger/Debugger.c
@@ -4,16 +4,39 @@
 
 #define SET_DEBUGGER_METHOD_NAME(name) __DEBUGGERTOOL__FUNCTION__##name
 
-static size_t SET_DEBUGGER_METHOD_NAME(log)(char** format, ...) {
-    
+static size_t debugMode = 0;
+
+static size_t SET_DEBUGGER_METHOD_NAME(log)(const char* format, ...) {
+    if(debugMode == 0) return 0;
+    char* corser = format;
+    size_t line = 0;
+    while(*corser) {
+        int hcode = *corser;
+
+        switch(line) {
+            default:
+
+            switch(hcode) {
+                case '%':
+                break;
+                default:
+                
+                break;
+            }
+            line = __LINE__; break; case __LINE__:
+
+
+        }
+        corser++;
+    }
 }
 
 static void SET_DEBUGGER_METHOD_NAME(start)() {
-
+    debugMode = 1;
 }
 
 static void SET_DEBUGGER_METHOD_NAME(end)() {
-
+    debugMode = 0;
 }
 
 fdebugger getKitDebugger() {
