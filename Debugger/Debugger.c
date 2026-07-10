37a392e15d2d7d4d2fee3c9878748a8e1c547cdf
armyUser3181 <yong3181.25.1@gmail.com>
2026-07-10 11:19:28 +0000

    goto

--- Debugger/Debugger.c
+++ Debugger/Debugger.c
@@ -4,6 +4,11 @@
 #include <string.h>
 
 #define SET_DEBUGGER_METHOD_NAME(name) __DEBUGGERTOOL__FUNCTION__##name
+#define DEBUG_LOG(format, args...) printf("%d:" format, __LINE__, args )
+#define DEBUG_ERROR_LOG(format, args...) DEBUG_LOG(" ERROR :" format, args)
+#define DEBUG_PUT(string) DEBUG_LOG("%s", string)
+#define DEBUG_ERROR_PUT(string) DEBUG_ERROR_LOG("%s", string)
+
 
 static struct {
     size_t debugMode;
@@ -44,16 +49,33 @@ static struct struct_put_struct get_put_struct(char* low, char* high) {
 static int debug_context_memory_write(char* low, char* high) {
     struct struct_put_struct unit = get_put_struct(low, high);
     if( unit.fall ) {
-        puts("error: out of memory in debugger ");
-        return 0;
+        DEBUG_ERROR_PUT("error: out of memory in debugger ");
+        return -1;
     }
-    
+    memcpy(unit.low, unit.ptr, unit.size);
     debug_context.memory.top += unit.size;
     return unit.size;
 }
 
-static int debug_context_memory_write_printf(char* low, char* high, ...) {
-
+static int debug_context_memory_write_printf(char* low, char* high, va_list list ) {
+    struct struct_put_struct unit = get_put_struct(low, high);
+    int count = 0; va_list list_cp; va_copy(list_cp, list);
+    char* space = debug_context.put.ptr;
+    size_t space_size = debug_context.put.size;
+    if( space_size < unit.size + 1 ) {
+        DEBUG_ERROR_PUT("error: out of memory in debugger ");
+        return -1;
+    }
+    memcpy(unit.low, space, unit.size);
+    space[unit.size] = '\0';
+    size_t input_size = vsnprintf(NULL, 0, space, list_cp);
+    va_end(list_cp);
+    if( unit.max_size < input_size + 1 ) {
+        DEBUG_ERROR_PUT("error: out of memory in debugger ");
+        return -1;
+    }
+    count = vsnprintf(unit.ptr, input_size + 1, space, list);
+    return count;
 }
 
 static int debug_printf_engine(char* ch) {
@@ -103,10 +125,6 @@ static int debug_printf_engine(char* ch) {
     return 0; // no print
 }
 
-
-
-
-
 static void SET_DEBUGGER_METHOD_NAME(setMemory)(char* ptr, size_t size) {
     debug_context.memory.ptr = ptr;
     debug_context.memory.size = size;
@@ -116,6 +134,7 @@ static void SET_DEBUGGER_METHOD_NAME(setMemory)(char* ptr, size_t size) {
 
 static int SET_DEBUGGER_METHOD_NAME(log)(const char* format, ...) {
     if(debug_context.debugMode == 0) return 0;
+    int count = 0;
     char* corser_low = (char*)format;
     char* corser_high = (char*)format;
     size_t line = 0;
@@ -131,8 +150,9 @@ static int SET_DEBUGGER_METHOD_NAME(log)(const char* format, ...) {
             }
             line = __LINE__; break; case __LINE__:
             switch( debug_printf_engine(hcode) ) {
-                case 2:
-                
+                case 2: { count += debug_context_memory_write(corser_low + 1, hcode); } break; case 1: {
+                    count += debug_context_memory_write_printf(corser_low, corser_high);
+                } break; case 0: continue;
             }
 
         }
